1e24f8e18c1d522a5326c65cefe3fffd7bac19f3
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-08 13:26:44 +0200

    I add the .splice method to the array to replace a value within the array.
    Print the array to the screen.

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -32,6 +32,15 @@ const teachers = [
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
+teachers.splice(4, 1, "Patrick")
+//tramite il metodo .splice determino -d-a quale indice eliminare, -quanti- elementi eliminare, e con cosa sostituirli
+console.log(teachers);
+//stampo a schermo l'array 
+
+
+
+
+
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
 
