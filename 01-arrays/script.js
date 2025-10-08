5ab0f0780c1dc91e1635413d66032d47e40613b6
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-08 14:59:28 +0200

    Change the type of the "isTeachersEmpty" variable from const to let
    Add a condition to check if array length = 0
    
    Set the "isTeachersEmpty" variable to "true" if the condition is met
    
    Add a printout for the "if" condition
    
    Add an "else" alternative to the condition
    
    Set the "isTeachersEmpty" variable to "false" if the "else" alternative is met
    
    Declare the "length" variable
    
    Add a printout for the "else" alternative

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -25,7 +25,7 @@ const teachers = [
     }
   }
 
-  console.log(fourthTeacher);
+  console.log(fourthTeacher)
   //stampo a schermo la variabile
 
   
@@ -46,10 +46,10 @@ const teachers = [
   const lastTeacher = teachers.pop()
   //tramite il metodo .pop rimuovo l'ultimo elemento e lo assegno alla variabile
 
-  console.log(lastTeacher);
+  console.log(lastTeacher)
   //stampo a schermo la variabile che ora contiene l'ultimo elemento estratto dall'array
 
-  console.log(teachers);
+  console.log(teachers)
   //l'array ora risulta mutato, ho un elemento in meno
 
 
@@ -74,7 +74,7 @@ const teachers = [
   teachers.push("Vanessa")
   //tramite il metodo .push aggiungo un elemento alla fine dell'array
 
-  console.log(teachers);
+  console.log(teachers)
   //stampo a schermo l'array
 
 
@@ -103,7 +103,30 @@ const lewisIndex = teachers.indexOf("Lewis")
 
 
 
-
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+
+  let isTeachersEmpty = null
+  //la variabile deve essere di tipo -let, per poter procere a riassegnazione del suo valore
+
+    if (teachers.length === 0){
+      //Condizione per verificare se la lunghezza di un array è pari a zero, in tal caso sarà vuoto
+
+      isTeachersEmpty = true
+      //assegnazione valore "true" in accordo con la casistica
+
+      console.log("L'array risulta vuoto")
+      //stampa del messaggio relativo al caso di array vuoto
+
+      }else{
+      //alternativa alla prima condizione  
+
+      isTeachersEmpty = false
+      //assegnazione valore "false" in accordo con la casistica
+
+      const lunghezza = teachers.length
+      //dichiaro varibile utile per la stampa a schermo
+
+      console.log(`L'array risulta contenere ${lunghezza} elementi`)
+      //stampa a schermo del messaggio relativo al caso di array contenente elementi 
+      }
