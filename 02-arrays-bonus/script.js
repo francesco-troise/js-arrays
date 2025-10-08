9829f1c6e44ac8f8012f100f6125ff39088a6b37
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-08 15:45:34 +0200

    Define "longNames" as an empty array
    Initialize the -for loop
    
    Add a variable "name" bound to the values ​​assumed by the index of the "teachers" array
    
    Add a condition to check the length of "name"
    
    Add a .push method to the longNames array if the condition is met
    
    Print the new "longNames" array to the screen

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -16,7 +16,7 @@ const teachers = [
   //cambio di valore, da "null" a "", per evitare che il valore "null" venga stampato
 
   for(i = teachers.length - 1; i >= 0; i--){
-  //ciclo for struttura per scorrere in "reverse" l'array
+  //ciclo -for strutturato per scorrere in "reverse" l'array
     reversedTeachers += teachers[i] + " "
     //ad ogni iterazione aggiungi alla varibile, concatenandoli, i valori presenti all indice 
   }
@@ -27,13 +27,25 @@ const teachers = [
 
 
 
-
-
-
-
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+const longNames = [];
+//cambio di valore da "null" a [], cosicchè sia bia un array inizialmente vuoto
+
+for(i = 0; i < teachers.length; i++){
+  let name = teachers[i]
+  //inizializzo variabile "name, che acquisce man mano i valore presenti nell'array teachers"
+
+  if(name.length >= 5)
+  //condizione per verificare se la lunghezza di "name"(che contiene man mano i valori dell array teachers) sia maggiore o uguale a 5
+    longNames.push(name)
+    //se la condizione è soddisfatta, tramite il motodo .push aggiungo lo specifico valore assunto da "name" all array "longNames"
+}
+console.log(longNames);
+
+
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
